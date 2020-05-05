363e150ee27d6ad7a3d180439f52bc1051fe9551
xkylesun <xyasun@gmail.com>
2020-05-05 16:58:39 -0700

    change

--- js/entry.js
+++ js/entry.js
@@ -4,8 +4,7 @@ require("../styles/00_reset.css");
 require("../styles/game.css");
 
 require('file-loader?name=[name].[ext]!../index.html');
-// require('file-loader?name=styles/[name].[ext]!../styles/game.css');
-// require('file-loader?name=styles/[name].[ext]!../styles/00_reset.css');
+
 
 window.addEventListener('load', () => {
     new Menu().start();
