cba49071271da35ba92e7f023835342a70d22fe6
xkylesun <xyasun@gmail.com>
2020-05-06 17:00:34 -0700

    new

--- js/util.js
+++ js/util.js
@@ -22,17 +22,17 @@ export const toCanvasY = (c, e) => {
         return posY;
 }
 
-export const addSound = (src) => {
-    this.sound = document.createElement("audio");
-    this.sound.src = src;
-    this.sound.setAttribute("preload", "auto");
-    this.sound.setAttribute("controls", "none");
-    this.sound.style.display = "none";
-    document.body.appendChild(this.sound);
-    this.play = function () {
-        this.sound.play();
-    }
-    this.stop = function () {
-        this.sound.pause();
-    }
-}
+// export const addSound = (src) => {
+//     this.sound = document.createElement("audio");
+//     this.sound.src = src;
+//     this.sound.setAttribute("preload", "auto");
+//     this.sound.setAttribute("controls", "none");
+//     this.sound.style.display = "none";
+//     document.body.appendChild(this.sound);
+//     this.play = function () {
+//         this.sound.play();
+//     }
+//     this.stop = function () {
+//         this.sound.pause();
+//     }
+// }
